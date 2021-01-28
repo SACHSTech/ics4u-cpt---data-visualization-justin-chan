e30b0208766438d81529c4f4f3bd4cf244a7d3a9
Ju-stinC <justin.chan21@ycdsbk12.ca>
2021-01-28 00:16:46 -0500

    extra after saving

---
+++ src/basic/YearFilter.java
@@ -0,0 +1,20 @@
+package basic;
+
+public class YearFilter {
+    
+    int yearChoice;
+
+
+    public void getYear(int whichYear) {
+
+        yearChoice = whichYear;
+
+    }
+
+    public int setYear() {
+
+        return yearChoice;
+
+    }
+
+}
