ca17404b67125034e554cc8f7d6455a1d9951c18
Ju-stinC <justin.chan21@ycdsbk12.ca>
2021-01-28 10:02:25 -0500

    added indiv records

--- src/basic/Main.java
+++ src/basic/Main.java
@@ -351,23 +351,11 @@ public class Main {
         IndivRecord ir = new IndivRecord();
 
         ir.countryList();
-        System.out.println("Which country would you like to view the individual record of? (only 2015, list above): ");
 
+        System.out.println("Which country would you like to view the individual record of? (only 2015, list above): ");
         ir.getCountryChoice(keyboard.readLine());
 
-        if(ir.setCountryChoice().equalsIgnoreCase("india")) {
-
-            ir.IndiaDetails();
-
-        }else if(ir.setCountryChoice().equalsIgnoreCase("uzbekistan")) {
-
-            ir.UzbekistanDetails();
-
-        }else if(ir.setCountryChoice().equalsIgnoreCase("cameroon")) {
-
-            ir.CameroonDetails();
-
-        }
-
+        ir.countryIfs();
+        
     }
 }
